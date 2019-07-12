28d06d2b62cadb15e86642767d3d5fb8b818d1ba
Alexander Magola <pustotnik@gmail.com>
2019-07-12 09:13:25 +0700

    update setup.py

--- setup.py
+++ setup.py
@@ -5,25 +5,35 @@ from setuptools import setup, find_packages
 #    long_description = fh.read()
 long_description = 'ZenMake - build system based on WAF'
 
+classifiers = """\
+Development Status :: 2 - Pre-Alpha
+License :: OSI Approved :: BSD License
+Environment :: Console
+Intended Audience :: Developers
+Programming Language :: Python
+Programming Language :: Python :: 2
+Programming Language :: Python :: 2.7
+Programming Language :: Python :: 3
+Programming Language :: Python :: 3.5
+Programming Language :: Python :: 3.6
+Programming Language :: Python :: 3.7
+Programming Language :: Python :: Implementation :: CPython
+Programming Language :: Python :: Implementation :: PyPy
+Operating System :: POSIX :: Linux
+Topic :: Software Development :: Build Tools
+"""
+
+classifiers = classifiers.splitlines()
+
 setup(
-    name='zenmake',
-    version='0.0.1',
-    description='ZenMake - build system based on WAF',
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url='https://github.com/pustotnik/zenmake',
-    author='Alexander Magola',
-    author_email='pustotnik@gmail.com',
-    packages=[],
-    classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'License :: OSI Approved :: BSD License',
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 3',
-        'Operating System :: POSIX :: Linux',
-        'Topic :: Software Development :: Build Tools',
-    ],
+    name = 'zenmake',
+    version = '0.0.2',
+    description = 'ZenMake - build system based on WAF',
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = 'https://github.com/pustotnik/zenmake',
+    author = 'Alexander Magola',
+    author_email = 'pustotnik@gmail.com',
+    packages = [],
+    classifiers = classifiers,
 )
