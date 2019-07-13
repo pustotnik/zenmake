4b77697936cb0246823f294a9ad3426e42574367
Alexander Magola <pustotnik@gmail.com>
2019-07-13 16:18:45 +0700

    try to run pytest on travis ci

---
+++ tests/test_autodict.py
@@ -0,0 +1,30 @@
+# coding=utf-8
+#
+
+# pylint: skip-file
+
+"""
+ Copyright (c) 2019, Alexander Magola. All rights reserved.
+ license: BSD 3-Clause License, see LICENSE for more details.
+"""
+
+import tests.common as cmn
+import zm.autodict
+
+class TestAutoDict(object):
+    def testAll(self):
+        d = zm.autodict.AutoDict()
+        d['test'] = 10
+        assert d == {'test' : 10}
+        assert hasattr(d, 'test')
+        assert d['test'] == d.test
+
+        assert 'something' not in d
+        d.something = 123
+        assert 'something' in d
+
+        assert not d.test2
+        assert not d['test3'].test4
+
+        d2 = zm.autodict.AutoDict(dict(a = 1, b =2))
+        assert d2 == dict(a = 1, b =2)
