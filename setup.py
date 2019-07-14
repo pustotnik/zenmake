91549b7c9e6e95ca21f8361d01200c29addbdb22
Alexander Magola <pustotnik@gmail.com>
2019-07-14 16:19:24 +0700

    update setup.py

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ Programming Language :: Python
 Programming Language :: Python :: 2
 Programming Language :: Python :: 2.7
 Programming Language :: Python :: 3
+Programming Language :: Python :: 3.4
 Programming Language :: Python :: 3.5
 Programming Language :: Python :: 3.6
 Programming Language :: Python :: 3.7
@@ -27,15 +28,18 @@ Topic :: Software Development :: Build Tools
 
 classifiers = classifiers.splitlines()
 
+python_requires = '>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, <4'
+
 setup(
     name = 'zenmake',
     version = '0.0.2',
     description = 'ZenMake - build system based on WAF',
     long_description = long_description,
     long_description_content_type = "text/markdown",
-    url = 'https://github.com/pustotnik/zenmake',
+    url = 'https://gitlab.com/pustotnik/zenmake',
     author = 'Alexander Magola',
     author_email = 'pustotnik@gmail.com',
     packages = [],
     classifiers = classifiers,
+    python_requires = python_requires,
 )
