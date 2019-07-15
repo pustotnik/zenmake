8f284102f66f51c41f610587da440ea208da32ad
Alexander Magola <pustotnik@gmail.com>
2019-07-15 11:48:54 +0700

    add guards to some *.h files in test projects

--- tests/projects/cpp/002-simple/util.h
+++ tests/projects/cpp/002-simple/util.h
@@ -1,2 +1,6 @@
+#ifndef __ZENMAKE_TEST_CPP_UTIL_H__
+#define __ZENMAKE_TEST_CPP_UTIL_H__
 
 void foo();
+
+#endif
--- tests/projects/cpp/003-withshlib/shlib/util.h
+++ tests/projects/cpp/003-withshlib/shlib/util.h
@@ -1,3 +1,5 @@
+#ifndef __ZENMAKE_TEST_CPP_UTIL_H__
+#define __ZENMAKE_TEST_CPP_UTIL_H__
 
 #ifdef _MSC_VER
     #define LIB_EXPORT __declspec(dllexport)
@@ -6,3 +8,5 @@
 #endif
 
 LIB_EXPORT void foo();
+
+#endif
