52521874744351ad902b1d2987042902aee62be3
Alexander Magola <pustotnik@gmail.com>
2019-09-08 19:46:07 +0700

    add zm.version

---
+++ zm/version.py
@@ -0,0 +1,10 @@
+# coding=utf-8
+#
+
+"""
+ Copyright (c) 2019, Alexander Magola. All rights reserved.
+ license: BSD 3-Clause License, see LICENSE for more details.
+"""
+
+#TODO: read version from git tags
+VERSION = '0.1.0'
