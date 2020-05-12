f2751ffd1efe9b07f558389b4abacf4aa21054c4
Alexander Magola <pustotnik@gmail.com>
2020-05-12 14:58:39 +0700

    fix 'monitlibs' for openbsd

--- src/zenmake/zm/waf/task.py
+++ src/zenmake/zm/waf/task.py
@@ -6,10 +6,15 @@
  license: BSD 3-Clause License, see LICENSE for more details.
 """
 
+import os
+
 from waflib import Task as WafTask, Node as WafNode, Errors as waferror
 from zm.constants import TASK_TARGET_KINDS, SYSTEM_LIB_PATHS
 from zm.utils import asmethod
 from zm.features import TASK_TARGET_FEATURES_TO_LANG
+
+listdir = os.listdir
+isfile = os.path.isfile
 
 APPLY_MONITLIBS_TASKTYPES = tuple(TASK_TARGET_KINDS)
 
@@ -17,6 +22,24 @@ def _getPathNode(bld, path):
     if not isinstance(path, WafNode.Node):
         return bld.root.find_node(path)
     return path
+
+def _findLibFileNode(pathNode, pattern, baseName, static, destOS):
+
+    filename = pattern % baseName
+
+    if not static and destOS == 'openbsd':
+        # Special case for OpenBSD: this system doesn't use symlinks *.so -> *.so.x.y
+        rootpath = pathNode.abspath()
+        for name in sorted(listdir(rootpath)):
+            if not name.startswith(filename):
+                continue
+            fullpath = '%s%s%s' % (rootpath, os.sep, name)
+            if isfile(fullpath):
+                return pathNode.make_node(name)
+
+        return None
+
+    return pathNode.find_node(filename)
 
 @asmethod(WafTask.Task, 'sig_explicit_deps', wrap = True, callOrigFirst = True)
 def _signatureExplicitDeps(self):
@@ -28,6 +51,7 @@ def _signatureExplicitDeps(self):
     taskgen = self.generator
     bld = taskgen.bld
     updateHash = self.m.update
+    destOS = self.env.DEST_OS
 
     for libsparam, targetkind in (('lib', 'shlib'), ('stlib', 'stlib')):
         libs = getattr(taskgen, 'monit%ss' % libsparam, None)
@@ -39,7 +63,7 @@ def _signatureExplicitDeps(self):
 
         lang = TASK_TARGET_FEATURES_TO_LANG[cname]
         pattern = self.env[lang + targetkind + '_PATTERN']
-        libs = [ pattern % lib for lib in libs]
+        isStaticLib = targetkind == 'stlib'
 
         for path in libpaths:
             path = _getPathNode(bld, path)
@@ -48,7 +72,7 @@ def _signatureExplicitDeps(self):
 
             notfound = []
             for lib in libs:
-                libnode = path.find_node(lib)
+                libnode = _findLibFileNode(path, pattern, lib, isStaticLib, destOS)
                 if libnode:
                     updateHash(libnode.get_bld_sig())
                 else:
