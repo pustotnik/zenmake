a4e2f1f7463b243155575c3f2267641e40c98c0d
Alexander Magola <pustotnik@gmail.com>
2021-11-03 17:44:14 +0700

    clean up pyutils

--- src/zenmake/zm/pyutils.py
+++ src/zenmake/zm/pyutils.py
@@ -5,46 +5,29 @@
  Copyright (c) 2019, Alexander Magola. All rights reserved.
  license: BSD 3-Clause License, see LICENSE for more details.
 
- Some python2/3 compatible stuffs. I don't use 'six' because I don't want
- to have extra dependencies. Also here can be some extra stuffs based on
- python only built-in ones.
+ Here are some extra stuff based on python only built-in ones.
 """
 
+#pylint: disable=invalid-name,undefined-variable,unused-import
+
 import sys
+from collections.abc import Mapping, MutableMapping
+maptype = Mapping
 
 PY_MAJOR_VER = sys.version_info[0]
 PY2 = PY_MAJOR_VER == 2
 PY3 = PY_MAJOR_VER >= 3
 
-#pylint: disable=wrong-import-position,missing-docstring
-#pylint: disable=invalid-name,undefined-variable,unused-import
+stringtype = str # pragma: no cover
+texttype = str # pragma: no cover
+binarytype = bytes # pragma: no cover
+_t = str # pragma: no cover
 
-if PY3:
-    stringtype = str # pragma: no cover
-    texttype = str # pragma: no cover
-    binarytype = bytes # pragma: no cover
-    _t = str # pragma: no cover
+def _unicode(s):
+    return s
 
-    def _unicode(s):
-        return s
-
-    def _encode(s):
-        return s
-
-else:
-    stringtype = basestring # pragma: no cover
-    texttype = unicode # pragma: no cover
-    binarytype = str # pragma: no cover
-    _t = unicode # pragma: no cover
-
-    def _unicode(s):
-        return unicode(s, 'utf-8', 'replace')
-
-    def _encode(s):
-        return s.encode('utf-8', 'replace')
-
-from collections.abc import Mapping, MutableMapping
-maptype = Mapping
+def _encode(s):
+    return s
 
 def struct(typename, attrnames):
     """
@@ -75,15 +58,15 @@ def struct(typename, attrnames):
     def __getattr__(self, name):
         """
         It will only get called for undefined attributes
-        and mostly to mute pylint 'no-member' warning
+        and exists here mostly to mute pylint 'no-member' warning
         """
         raise self.__getattribute__(name)
 
     namespace = {
-        '__doc__': '%s(%s)' % (typename, attrnames),
-        '__slots__' : attrnames,
-        '__init__' : __init__,
-        '__repr__' : __repr__,
+        '__doc__'    : '%s(%s)' % (typename, attrnames),
+        '__slots__'  : attrnames,
+        '__init__'   : __init__,
+        '__repr__'   : __repr__,
         '__getattr__': __getattr__,
     }
     result = type(typename, (object,), namespace)
