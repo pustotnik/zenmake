432d959429faeea36175d4730d5c8dbb5c78c6ee
Alexander Magola <pustotnik@gmail.com>
2022-02-20 15:38:52 +0700

    make script to install qt on windows on github actions

---
+++ scripts/gh-install-qt-on-windows.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -ex
+
+python -m pip install --upgrade pip
+python -m pip install --upgrade aqtinstall
+# there is no win64_msvc2019_64 for Qt 5.12.12
+aqt install-qt --outputdir C:/Qt windows desktop 5.15.2 win64_msvc2019_64
+aqt install-qt --outputdir C:/Qt windows desktop 5.15.2 win64_mingw81
+# aqt install-qt --outputdir C:/Qt windows desktop 5.12.12 win64_msvc2017_64
